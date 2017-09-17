853e07d7115c9868e1a7de08af59158e3e89f498
Gene Williams <GeneWillb@Gmail.com>
2017-09-17 11:52:08 -0400

    working on having gifs go from still to moving and back

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,12 @@ var topics = ["Spongebob", "Pepe", "Doggo", "Cato", "Bamboozled"];
 
 var topic;
 
+var imgUrl;
+
+var image;
+
+var results;
+
 $(document).ready(function() {
     renderButton();
 });
@@ -14,29 +20,70 @@ function renderButton() {
     }
 }
 
-
-$(document).on("click", "button", function() {
+$(document).on("click", ".button", function() {
+    $("#gifs").empty();
     topic = $(this).text();
     console.log(topic);
+    
     
     //api key
     var APIKey = "b5b598ce7fb8471882da248d11e64361";
 
-//add giphy aip url
+    //giphy api url
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&limit=10&q=" +topic;
-    
-    
     
     $.ajax({
         url: queryURL,
         method: "GET"
     }).done(function(response) {
         console.log(response);
+        
+        results = response.data;
+        
+        for (var i = 0; i < results.length; i++) {
+            
+            var resultDiv = $("<div>");
+            
+            imgUrl = results[i].images.fixed_height_still.url;
+    
+            var p = $("<p>");
+    
+            image = $("<img>");
+    
+            p.text("Rating: " + results[i].rating);
+    
+            image.attr("src", imgUrl);
+            image.data("state", "still");
+            
+            resultDiv.append(p);
+            resultDiv.append(image);
+            
+            $("#gifs").prepend(resultDiv);
+            
+            
+        }
+        console.log("button click");
     });
 });
 
+$(document).on("click", "img", function() {
+    if (image.data === "still") {
+        imgUrl = results[i].images.fixed_height.url;
+        state = "animate";
+    }
+    else {
+        imgUrl = results[i].images.fixed_height_still.url;
+        state = "still"
+    }
+    console.log("gif was clicked");
+});
+
+
+
+//adds new buttons
 $("#submit").on("click", function() {
     $("#buttons").empty();
+    $("#gifs").empty();
    var newTopic = $("#new-topic").val();
    topics.push(newTopic);
    if (topics.indexOf(newTopic) > -1) {
